0131e23f3ca60287b51c6e04baf1701d9ce0f394
Kelly Selden <kellyselden@gmail.com>
2017-10-25 18:50:26 -0700

    update ember-cli 2.16.2

--- addon/components/each-of.js
+++ addon/components/each-of.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';
+import Component from '@ember/component';
 import layout from '../templates/components/each-of';
 
-const MyComponent = Ember.Component.extend({
+const MyComponent = Component.extend({
   layout,
 
   tagName: ''
