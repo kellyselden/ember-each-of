abc61edcd643ddbac8c9f1d5aadddc22adb29eb4
Kelly Selden <kellyselden@gmail.com>
2018-03-31 09:43:41 -0700

    run ember-qunit-codemod (#22)

--- tests/integration/components/each-of-test.js
+++ tests/integration/components/each-of-test.js
@@ -1,38 +1,40 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('each-of', 'Integration | Component | each of', {
-  integration: true
-});
+module('Integration | Component | each of', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it is tagless', function(assert) {
-  this.render(hbs`
-    {{each-of}}
-  `);
+  test('it is tagless', async function(assert) {
+    await render(hbs`
+      {{each-of}}
+    `);
 
-  assert.equal(this.$().html().trim(), '<!---->');
-});
+    assert.equal(this.$().html().trim(), '<!---->');
+  });
 
-test('it iterates and destructures', function(assert) {
-  this.set('items', [[1, 2], [3, 4]]);
+  test('it iterates and destructures', async function(assert) {
+    this.set('items', [[1, 2], [3, 4]]);
 
-  this.render(hbs`
-    {{#each-of items as |first second|}}
-      {{first}} {{second}}
-    {{/each-of}}
-  `);
+    await render(hbs`
+      {{#each-of items as |first second|}}
+        {{first}} {{second}}
+      {{/each-of}}
+    `);
 
-  assert.equal(this.$().text().trim().replace(/\n/g, '').replace(/ {2,}/g, ' '), '1 2 3 4');
-});
+    assert.equal(this.$().text().trim().replace(/\n/g, '').replace(/ {2,}/g, ' '), '1 2 3 4');
+  });
 
-test('it handles up to ten items', function(assert) {
-  this.set('items', [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]);
+  test('it handles up to ten items', async function(assert) {
+    this.set('items', [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]);
 
-  this.render(hbs`
-    {{#each-of items as |one two three four five six seven eight nine ten|}}
-      {{one}} {{two}} {{three}} {{four}} {{five}} {{six}} {{seven}} {{eight}} {{nine}} {{ten}}
-    {{/each-of}}
-  `);
+    await render(hbs`
+      {{#each-of items as |one two three four five six seven eight nine ten|}}
+        {{one}} {{two}} {{three}} {{four}} {{five}} {{six}} {{seven}} {{eight}} {{nine}} {{ten}}
+      {{/each-of}}
+    `);
 
-  assert.equal(this.$().text().trim(), '1 2 3 4 5 6 7 8 9 10');
+    assert.equal(this.$().text().trim(), '1 2 3 4 5 6 7 8 9 10');
+  });
 });
